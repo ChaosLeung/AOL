06ed28fb77a038b7692b999a51507107a64924a5
Chaos Leung <lgf42031@gmail.com>
2021-01-04 15:56:17 +0800

    Use object declaration for Jvmti

--- library-jvmti/src/main/java/com/chaos/library/aol/jvmti/Jvmti.kt
+++ library-jvmti/src/main/java/com/chaos/library/aol/jvmti/Jvmti.kt
@@ -9,63 +9,61 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.Paths
 
-class Jvmti {
+object Jvmti {
 
-    companion object {
-        private const val TAG = "Jvmti"
+    private const val TAG = "Jvmti"
 
-        private const val AGENT_NAME = "aol-jvmti-agent"
+    private const val AGENT_NAME = "aol-jvmti-agent"
 
-        private const val UNKNOWN_SIZE = -1L
+    private const val UNKNOWN_SIZE = -1L
 
-        private fun isJvmtiSupport(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+    private fun isJvmtiSupport(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
 
-        private var attached: Boolean = false
+    private var attached: Boolean = false
 
-        @SuppressLint("SoonBlockedPrivateApi")
-        fun attach(context: Context) {
-            if (attached || !isJvmtiSupport()) {
-                return
-            }
-
-            attached = true
-
-            val originPath = context.applicationInfo.nativeLibraryDir + File.separatorChar + "lib" + AGENT_NAME + ".so"
-            val targetDir = File(context.filesDir, "jvmti")
-            if (!targetDir.exists()) {
-                targetDir.mkdirs()
-            }
-            val targetFile = File(targetDir, "$AGENT_NAME.so")
-            if (targetFile.exists()) {
-                targetFile.delete()
-            }
-            val agentLibPath = targetFile.absolutePath
-            Files.copy(Paths.get(originPath), Paths.get(agentLibPath))
-
-            // TODO: 当前版本已经 copy 过后面不需要再 copy
-
-            Log.d(TAG, "attach: $agentLibPath")
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                Debug.attachJvmtiAgent(agentLibPath, null, context.classLoader)
-            } else {
-                val vmDebugClass = Class.forName("dalvik.system.VMDebug")
-                val attachAgentMethod = vmDebugClass.getDeclaredMethod("attachAgent", String::class.java)
-                    .apply { isAccessible = true }
-                attachAgentMethod.invoke(null, agentLibPath)
-            }
-
-            System.loadLibrary(AGENT_NAME)
+    @SuppressLint("SoonBlockedPrivateApi")
+    fun attach(context: Context) {
+        if (attached || !isJvmtiSupport()) {
+            return
         }
 
-        fun getObjectSize(o: Any): Long {
-            if (!isJvmtiSupport()) {
-                return UNKNOWN_SIZE
-            }
-            return nativeGetObjectSize(o)
+        attached = true
+
+        val originPath = context.applicationInfo.nativeLibraryDir + File.separatorChar + "lib" + AGENT_NAME + ".so"
+        val targetDir = File(context.filesDir, "jvmti")
+        if (!targetDir.exists()) {
+            targetDir.mkdirs()
+        }
+        val targetFile = File(targetDir, "$AGENT_NAME.so")
+        if (targetFile.exists()) {
+            targetFile.delete()
+        }
+        val agentLibPath = targetFile.absolutePath
+        Files.copy(Paths.get(originPath), Paths.get(agentLibPath))
+
+        // TODO: 当前版本已经 copy 过后面不需要再 copy
+
+        Log.d(TAG, "attach: $agentLibPath")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            Debug.attachJvmtiAgent(agentLibPath, null, context.classLoader)
+        } else {
+            val vmDebugClass = Class.forName("dalvik.system.VMDebug")
+            val attachAgentMethod = vmDebugClass.getDeclaredMethod("attachAgent", String::class.java)
+                .apply { isAccessible = true }
+            attachAgentMethod.invoke(null, agentLibPath)
         }
 
-        @JvmStatic
-        private external fun nativeGetObjectSize(o: Any): Long
+        System.loadLibrary(AGENT_NAME)
     }
+
+    fun getObjectSize(o: Any): Long {
+        if (!isJvmtiSupport()) {
+            return UNKNOWN_SIZE
+        }
+        return nativeGetObjectSize(o)
+    }
+
+    @JvmStatic
+    private external fun nativeGetObjectSize(o: Any): Long
 }
