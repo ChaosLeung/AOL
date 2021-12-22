0e429a88d7c95690266b4987d053c852e877d097
Chaos <lgf42031@gmail.com>
2021-12-22 09:45:46 +0800

    [cli] support array class

--- cli/src/main/java/com/chaos/aol/cli/ops/ClassArgOp.kt
+++ cli/src/main/java/com/chaos/aol/cli/ops/ClassArgOp.kt
@@ -1,8 +1,15 @@
 package com.chaos.aol.cli.ops
 
+import java.lang.reflect.Array
+
 abstract class ClassArgOp : Op {
     override fun run(vararg args: String) {
-        runWithClass(Class.forName(args[0]))
+        val str = args[0].trim()
+        if (str.lastIndexOf("[]") == str.length - 2) {
+            runWithClass(Array.newInstance(Class.forName(str.substring(0, str.length - 2)), 0)::class.java)
+        } else {
+            runWithClass(Class.forName(args[0]))
+        }
     }
 
     abstract fun runWithClass(clazz: Class<*>)
